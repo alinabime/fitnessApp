de78f93671a80c9d540008774ce0d85ce7d85485
Ali <alinabime@gmail.com>
2025-03-29 09:11:34 +0330

    Initial Commit

---
+++ fitnessApp/fitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessAppApp.swift
+//  fitnessApp
+//
+//  Created by alinabi on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct fitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
